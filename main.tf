cf6527fc7b3e2f8199a41ed6bacffd2cda223eb8
valeriuhudea <33030607+valeriuhudea@users.noreply.github.com>
2023-11-06 22:11:19 +0200

    Start tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+variable "org_name" {}
+variable "api_token" {}
+variable "base_url" {}
+
+# Enable and configure the Okta provider
+terraform {
+  required_providers {
+    okta = {
+      source  = "okta/okta"
+      version = "~> 4.6.1"
+    }
+  }
+}
+
+provider "okta" {
+    org_name = var.org_name
+    base_url = var.base_url
+    api_token = var.api_token
+}
